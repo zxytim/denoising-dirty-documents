6819785164e52261764b68b03569470205108985
Xinyu Zhou <zxy@megvii.com>
2015-09-30 11:54:01 +0800

    pack output

--- predict_images_all.sh
+++ predict_images_all.sh
@@ -4,15 +4,23 @@
 
 set  -x
 
+make
+
 config_file="$1"
 bname=$(basename "$config_file")
 name=${bname%.*}
 
 for dataset in train val test; do
-	echo ./theano gpu4 \
+	out_dir=out/${name}/${dataset}
+	echo "./theano gpu4 \
 		./predict_images.py \
 		--image_list data/${dataset}.list \
 		--config $config_file \
 		--dump_prefix train_log/${name}/min_val_loss \
-		--output_dir out/${name}/${dataset}
+		--output_dir $out_dir"
 done | parallel bash -c {}
+
+
+csv_path=out/${name}.test.csv
+zip_path=${csv_path}.test.zip
+./images2csv out/${name}/test $csv_path && 7z a $zip_path $csv_path && rm $csv_path
