f1923a6270a223b5c897fc60592a219fbe49916f
Xinyu Zhou <zxy@megvii.com>
2015-10-01 10:59:18 +0800

    update update_results.sh

--- update_results.sh
+++ update_results.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in out/*; do [ -d $i ] && echo ./predict_images_all.sh config/$(basename $i).py; done | parallel bash -c {}
+for i in train_log/*; do [ -d $i ] && echo ./predict_images_all.sh config/$(basename $i).py; done | parallel bash -c {}
